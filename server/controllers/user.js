b0761f51441d338fd6e6ab36263cb8ca7afe3840
SahiraTejada <Sahira2208@hotmail.com>
2022-09-22 14:09:58 -0400

    Controllers User

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,43 @@
-export const test =(req,res) =>{
-    res.json('users....')
+import { createError } from "../error.js"
+import User from "../models/User.js"
+
+export const update = async (req,res,next) =>{
+   if(req.params.id === req.user.id){
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+            $set:req.body
+        }
+        ,{new : true} //Retornar la version mas nueva
+        )
+        res.status(200).json(updatedUser)
+        
+    }catch(err){
+        next(err)
+    }
+
+   }else{
+    return next(createError(403,'Only update your account'))
+   }
+}
+
+export const deleteUser = (req,res,next) =>{
+   
+}
+
+export const getUser =(req,res,next) =>{
+   
+}
+
+export const subscribe =(req,res,next) =>{
+   
+}
+export const unsubscribe =(req,res,next) =>{
+   
+}    
+
+export const like =(req,res,next) =>{
+   
+}
+export const dislike =(req,res,next) =>{
+   
 }
---
+++ server/verifyToken.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+import { createError } from "./error.js"
+
+export  const verifyToken = (req,res,next) =>{
+    const token = req.cookies.access_token
+    if(!token) return next(createError(401,'you are not authenticated'));
+
+    jwt.verify(token,process.env.JWT,(err,user)=>{
+        if(err) return next(createError(403,'Token not valid!'));
+        req.user = user;
+        //Despues de hacer la verificacion va a continuar donde se quedo ya sea en borrar usuario, actualizar.....
+        next();
+    });
+}
