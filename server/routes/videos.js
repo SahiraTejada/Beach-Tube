248c1c0f4360b9dc7bd3818d5a0cb0eca453bd91
SahiraTejada <Sahira2208@hotmail.com>
2022-09-22 18:34:30 -0400

    Begin video route and controllers

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import {  } from '../controllers/video.js';
+import { addVideo, deleteVideo, getVideo, updateVideo } from '../controllers/video.js';
+import { verifyToken } from '../verifyToken.js';
 
 const router = express.Router();
 
-
+//Create a video
+router.post('/',verifyToken,addVideo)
+router.put('/:id',verifyToken,updateVideo)
+router.delete('/:id',verifyToken,deleteVideo)
+router.get('/find/:id',verifyToken,getVideo)
 
 export default router;
