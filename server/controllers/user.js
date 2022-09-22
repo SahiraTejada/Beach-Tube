eab3afff3104886e9bed81ecf75f21768f23ab0e
SahiraTejada <Sahira2208@hotmail.com>
2022-09-22 14:20:23 -0400

    Begin delete user and token in user routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -35,20 +35,75 @@ export const deleteUser = async (req,res,next) =>{
    }
 }
 
-export const getUser =(req,res,next) =>{
-   
+export const getUser = async(req,res,next) =>{
+   if(req.params.id === req.user.id){
+    try{
+         await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted")
+        
+    }catch(err){
+        next(err)
+    }
+
+   }else{
+    return next(createError(403,'Only delete your account'))
+   }
 }
 
-export const subscribe =(req,res,next) =>{
-   
+export const subscribe = async (req,res,next) =>{
+   if(req.params.id === req.user.id){
+    try{
+         await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted")
+        
+    }catch(err){
+        next(err)
+    }
+
+   }else{
+    return next(createError(403,'Only delete your account'))
+   }
 }
-export const unsubscribe =(req,res,next) =>{
-   
+export const unsubscribe = async(req,res,next) =>{
+   if(req.params.id === req.user.id){
+    try{
+         await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted")
+        
+    }catch(err){
+        next(err)
+    }
+
+   }else{
+    return next(createError(403,'Only delete your account'))
+   }
 }    
 
-export const like =(req,res,next) =>{
-   
+export const like = async (req,res,next) =>{
+   if(req.params.id === req.user.id){
+    try{
+         await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted")
+        
+    }catch(err){
+        next(err)
+    }
+
+   }else{
+    return next(createError(403,'Only delete your account'))
+   }
 }
-export const dislike =(req,res,next) =>{
-   
+export const dislike =  async(req,res,next) =>{
+   if(req.params.id === req.user.id){
+    try{
+         await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted")
+        
+    }catch(err){
+        next(err)
+    }
+
+   }else{
+    return next(createError(403,'Only delete your account'))
+   }
 }
