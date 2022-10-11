14c1b25ad9340062fa72758ac08545ac9efe49af
SahiraTejada <Sahira2208@hotmail.com>
2022-10-11 10:50:03 -0400

    Begin Comments add

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -3,7 +3,8 @@ import {useState,useEffect} from 'react';
 import { useSelector } from 'react-redux';
 import Comment from './Comment';
 import axios from 'axios';
-import UserDefault from '../imgs/user.png'
+import UserDefault from '../imgs/user.png';
+import { useLocation } from 'react-router-dom';
 const Container = styled.div`
 
 `;
@@ -33,12 +34,37 @@ font-weight: 400;
 line-height: 20px;
 margin:5px;
 `;
+
+const Button= styled.button`
+background-color:${(props) => props.type === "cancel" ? "transparent":" rgba(255, 255, 255, 0.1)"};
+color:#aaa;
+ text-transform:uppercase;
+ border:none;
+ border-radius:2px;
+ height:max-content;
+ padding:10px 16px;
+ cursor:pointer;
+ font-weight:700;
+ letter-spacing: 0.5px;
+
+`
+const Buttons = styled.div`
+display:flex;
+float:right;
+margin-top:5px;
+
+
+`;
 const Comments = ({videoId}) => {
 
   const { currentUser } = useSelector((state) => state.user);
-
+  const { currentVideo } = useSelector((state) => state.video);
   const [comments, setComments] = useState([]);
-
+  const [show, setShow] = useState(false);
+  const [desc, setDesc] = useState("");
+  const [newComment, setNewComment] = useState({});
+      const [CommentLists, setCommentLists] = useState([])
+  const path = useLocation().pathname.split("/")[2];
   useEffect(() => {
     const fetchComments = async () => {
       try {
@@ -48,15 +74,32 @@ const Comments = ({videoId}) => {
     };
     fetchComments();
   }, [videoId]);
+    
 
+const handleComment = async ()=>{
+ 
+   try{
+      
+        const res = await axios.post(`/comments/${videoId}`,{desc,videoId});
+        setComments(res.data);
+   }catch(err){
+    console.log('vnjfibv8978776');
+   }
+ 
+
+}
   //TODO: ADD NEW COMMENT FUNCTIONALITY
-
+console.log(videoId);
   return (
     <Container>
       <NewComment>
          <Avatar src={currentUser.img ? (currentUser.img): (UserDefault)} />
-        <Input placeholder="Add a comment..." />
-      </NewComment>
+        <Input onClick={()=> setShow(true)} onChange={(e) => setDesc(e.target.value)}
+        placeholder="Add a comment..." />
+       
+      </NewComment> {
+       show ?  <Buttons><Button  onClick={()=> setShow(false)} type="cancel">Cancelar </Button><Button onClick={handleComment}>Comentar</Button></Buttons>: null
+        }
       {comments.map(comment=>(
         <Comment key={comment._id} comment={comment}/>
       ))}
